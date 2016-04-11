e68947fd610e2c68d26a64e4e254fc1af998d242
snart <ariannaosar@gmail.com>
2016-04-10 21:18:35 -0500

    added ccipher page request
    there are probably syntax errors everywhere. too used to java

--- webRequest.py
+++ webRequest.py
@@ -1,18 +1,28 @@
 import urllib2
+import requests
 from HTMLParser import HTMLParser
-
+def main():
+	email= "uremail"
+	passwd= "urpass"
+	allhtml = readHTML(email,passwd)
+	codehtml = parser(HTMLParser, allhtml)
+	caesarCiph()
 #manually enter your email and password before you run it
 #r = requests.get('http://game.hackdfw.com/play/puzzles',auth=('username/email','pass'))
 
 #retrieves the html from the webpage using urllib
-def readHTML():
-	feed = urllib.urlopen('http://game.hackdfw.com/play/puzzles',auth=('username/email','pass'))
+def readHTML(email,passwd):
+	feed = urllib.urlopen('http://game.hackdfw.com/play/puzzles',auth=(email,passwd))
 	html = feed.read()
 	return html
 
 #@param stream array of strings
+#**********might not need this anymore
 def extractInfo(stream):
-	index = stream.get('')
+	info = []
+	index = stream.get('b') #get bolded text; the codes are bolded
+	for i in range(0,b.length,i=i+2): #get every other index
+		info.append(stream[index+1]) #appends all bolded content
 	
 
 # makes a subclass of HTMLParser and uses it to 
@@ -26,19 +36,19 @@ def parser(HTMLParser,htmlText):
 	class MyHTMLParser(HTMLParser):
 	  htmlStream = []
 	    # create a subclass and override the handler methods
-
-	  def handle_starttag(self, tag, attrs):
-	      htmlStream.append(tag)
-	  def handle_endtag(self, tag):
-	      htmlStream.append(tag)
 	  def handle_data(self, data):
-	      htmlStream.append(data)
+	  	if class=='code' and tag=='b': #the tag thing might throw an error here
+	  		htmlStream.append(data) #should theoretically append all the codes
 
 	    # instantiate the parser
 	    parser = MyHTMLParser()
 	    parser.feed(htmlText)
+	    return htmlStream
+	
 
-
-def caesarCiph():
+def caesarCiph(key,email,counter):
   # send info through http://eliyazdi.github.io/decode/
   # yeah i know its cheating but i really dont feel like reinventing the wheel here
+  payload = payload{'key':key,'email':email,'dabutton':click}
+  r = request('http://eliyazdi.github.io/decode/',params=payload)
+  print "Request %s sent" % counter #assuming python 2
